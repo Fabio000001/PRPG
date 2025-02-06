c56a32e86ed8c8bf678056017f11154d6724f0e3
fabioacosta <fabioacosta00001@gmail.com>
2025-02-06 20:03:11 +0000

    Bugfender instalado

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import { Game } from 'phaser';
 import { GameOver } from './scenes/GameOver';
 import { MainMenu } from './scenes/MainMenu';
 import { Preloader } from './scenes/Preloader';
+import { Bugfender } from '@bugfender/sdk';
+
+Bugfender.init({
+    appKey: "51xntK1ezjJhyy2JTQJyl1EwePsEJWsZ",
+});
 
 //  Find out more information about the Game Config at: https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config = {
